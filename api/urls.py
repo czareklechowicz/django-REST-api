4613afb193517cc8a9e0f0a4739fba044e75afbd
czareklechowicz <czareklechowicz2000@wp.pl>
2023-01-15 15:35:39 +0100

    "in progress" Signed-off-by: czareklechowicz <czareklechowicz2000@wp.pl>

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
-
-from django.contrib import admin
-from django.urls import path
-
 from django.urls import include, path
-from api import urls
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 
 urlpatterns = [
-    path('api/', include(urls)),
-    path('admin/', admin.site.urls)
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
