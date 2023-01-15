cb7be0a1ccc065bcd274a36997930f6723f68a29
czareklechowicz <czareklechowicz2000@wp.pl>
2023-01-15 15:37:20 +0100

    "."
    Signed-off-by: czareklechowicz <czareklechowicz2000@wp.pl>

--- api/urls.py
+++ api/urls.py
@@ -9,5 +9,4 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
