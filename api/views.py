1025298e6a23b027b3b1e00fbf06cca969817935
czareklechowicz <czareklechowicz2000@wp.pl>
2023-01-18 17:17:00 +0100

    "in progress"
    Signed-off-by: czareklechowicz <czareklechowicz2000@wp.pl>

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from api.serializers import UserSerializer
-from models import Cars
+from .models import Cars
 from .serializers import CarsSerializer
 
 
