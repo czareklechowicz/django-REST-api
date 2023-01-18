26db2636240ce263f8eb27f22b49594763507755
czareklechowicz <czareklechowicz2000@wp.pl>
2023-01-18 16:46:46 +0100

    "in progress"
    Signed-off-by: czareklechowicz <czareklechowicz2000@wp.pl>

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class cars(models.Model):
+    brand = models.CharField(max_length=32),
+    model = models.CharField(max_length=32),
+    year = models.BooleanField(default = False),
+    car_mileage = models.CharField(max_length=32),
+    horse_power = models.CharField(max_length=32),
+    description = models.TextField(max_length=256),
+
