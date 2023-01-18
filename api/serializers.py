84e54c4f2901993f37ec4538fad41bed4fc143b7
czareklechowicz <czareklechowicz2000@wp.pl>
2023-01-18 17:06:08 +0100

    "in progress"
    Signed-off-by: czareklechowicz <czareklechowicz2000@wp.pl>

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Cars
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+
+class CarsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Cars
+        fields = ['brand', 'model', 'year', 'car mileage', 'horse power', 'description']
--- api/views.py
+++ api/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from api.serializers import UserSerializer
+from models import Cars
+from .serializers import CarsSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
+class CarsViewSet(viewsets.ModelViewSet):
+    queryset = Cars.objects.all().order_by('-date_joined')
+    serializer_class = CarsSerializer
