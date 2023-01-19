59f34ec1d771e691d720f2917343566629e7f3da
czareklechowicz <czareklechowicz2000@wp.pl>
2023-01-19 13:44:06 +0100

    "in progress"
    Signed-off-by: czareklechowicz <czareklechowicz2000@wp.pl>

--- api/models.py
+++ api/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Cars(models.Model):
-    brand = models.CharField(max_length=32),
-    model = models.TextField(max_length=32),
-    year = models.BooleanField(default = False),
-    kilometers = models.TextField(max_length=32),
-    horse_power = models.TextField(max_length=32),
-    description = models.TextField(max_length=256),
+    brand = models.CharField(max_length=32)
+    model = models.CharField(max_length=32)
+    year = models.BooleanField(default = False)
+    kilometers = models.TextField(max_length=32)
+    horse_power = models.TextField(max_length=32)
+    description = models.TextField(max_length=256)
 
--- warehouse/urls.py
+++ warehouse/urls.py
@@ -1,10 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-
 from django.urls import include, path
 from api import urls
+import sys
+sys.path.insert(1, '/Users/czareklechowicz/cars_warehouse/api/urls.py')
+
+
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include(urls)),
-    path('admin/', admin.site.urls)
 ]
